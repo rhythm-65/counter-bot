f15ac0de7125d78c24b28ecf2260c9692f5d41b6
rhythm-65 <118065878+rhythm-65@users.noreply.github.com>
2023-10-01 06:23:48 +0530

    Add files via upload

---
+++ counter-bot.py
@@ -0,0 +1,11 @@
+sentence = input("Enter a sentence or paragraph: ")
+
+# Counting words
+word_count = len(sentence.split())
+
+# Counting characters
+char_count = len(sentence)
+
+
+print("Number of words:", word_count)
+print("Number of characters:", char_count)
